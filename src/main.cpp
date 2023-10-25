d5325fea43da290b7cb1d814854fee7e6bc76bb0
camila314 <47485054+camila314@users.noreply.github.com>
2023-10-24 23:20:59 -0500

    uibuilder yay yippee

--- src/main.cpp
+++ src/main.cpp
@@ -1,100 +1,85 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/LevelInfoLayer.hpp>
-#include <Geode/modify/LevelCell.hpp>
+#include <Geode/modify/CustomListView.hpp>
+#include <UIBuilder.hpp>
 
 #include <random>
 
 using namespace geode::prelude;
 
-int randomSeededNumber(int levelID) {
-  std::mt19937 rng(levelID);
-  std::uniform_int_distribution<int> random(0, 100);
-
-  return random(rng);
+// https://crypto.stackexchange.com/a/16231
+uint32_t levelHash(uint32_t x) {
+    int n = 12;
+    do // repeat for n from 12 downto 1
+        x = ((x>>8)^x)*0x6B+n;
+    while( --n!=0 );
+    return x % 100;
 }
 
-constexpr std::array<std::string, 5> pronounList = {
+char const* pronounList[] = {
     "He/Him",
     "She/Her",
     "They/Them",
     "It/Its",
+    "He/They",
+    "She/They",
 	"Any/All"
 };
 
-constexpr std::array<std::string, 4> neopronounList = {
+char const* neopronounList[] = {
     "Ze/Hir",
+    "Ze/Zir",
     "Xe/Xem",
     "E/Eir",
+    "Ey/Em",
     "Per/Per",
+    "Fae/Faer"
 };
 
-constexpr std::string getPronoun(int levelID) {
+constexpr char const* getPronoun(int levelID) {
+	size_t index = levelHash(levelID) % (sizeof(pronounList) / sizeof(char*));
+
 	// Due to the fact that neo-pronouns are less common, we'll make them less likely to appear.
-	if (randomSeededNumber(levelID) < 20 && Mod::get()->getSettingValue<bool>("neopronouns")) {
-		return neopronounList[randomSeededNumber(levelID) % neopronounList.size()];
-	}
-
-	return pronounList[randomSeededNumber(levelID) % pronounList.size()];
+	if (levelHash(levelID) < 20 && Mod::get()->getSettingValue<bool>("neopronouns"))
+		return neopronounList[index];
+	else
+		return pronounList[index];
 };
-
-
-Result<CCLabelBMFont*> getLabelFromText(CCNode* node, std::string text) {
-	auto children = CCArrayExt<CCNode*>(node->getChildren());
-	for (auto child : children) {
-		if (auto label = safe_cast<CCLabelBMFont*>(child)) {
-			auto labelString = std::string(label->getString());
-			if (!labelString.compare(text)) {
-				return Ok(label);
-			}
-		}
-	}
-	return Err("Label not found");
-}
 
 class $modify(LevelInfoLayer) {
 	bool init(GJGameLevel* level) {
-		if (!LevelInfoLayer::init(level)) {
+		if (!LevelInfoLayer::init(level))
 			return false;
-		}
-		auto levelNameResult = getLabelFromText(this, level->m_levelName);
-		
-		if (levelNameResult.isErr()) {
-			return false;
-		}
 
-		auto levelNameLabel = levelNameResult.unwrap();
-
-		std::string pronoun = getPronoun(level->m_levelID);
-		
-		auto pronounLabel = CCLabelBMFont::create(pronoun.c_str(), "bigFont.fnt");
-		pronounLabel->setScale(0.4);
-		pronounLabel->setPosition(levelNameLabel->getPosition() + ccp(0, -levelNameLabel->getScaledContentSize().height / 2 - 4));
-		pronounLabel->setID("pronoun-label");
-		this->addChild(pronounLabel);
+		Build<CCLabelBMFont>::create(getPronoun(level->m_levelID), "bigFont.fnt")
+			.scale(0.4)
+			.center()
+			.posY(CCDirector::sharedDirector()->getWinSize().height - 36)
+			.id("pronoun-label")
+			.parent(this);
 
 		// Move "By <username>" down a bit
-		auto creatorLabel = this->getChildByID("creator-info-menu")->getChildByID("creator-name");
-		creatorLabel->setPosition(creatorLabel->getPosition() + ccp(0, -7));
+		Build(this->getChildByIDRecursive("creator-name")).move(0, -7);
 
 		return true;
 	}
 };
 
+class $modify(CustomListView) {
+	void loadCell(TableViewCell* tvc, int i) {
+		CustomListView::loadCell(tvc, i);
 
-class $modify(LevelCell) {
-	void loadFromLevel(GJGameLevel* level) {
-		// call original method
-		LevelCell::loadFromLevel(level);
+		if (auto lc = typeinfo_cast<LevelCell*>(tvc)) {
+			auto pronoun = fmt::format("({})", getPronoun(lc->m_level->m_levelID));
 
-		std::string pronoun = getPronoun(level->m_levelID);
-
-		auto pronounLabel = CCLabelBMFont::create(std::format("({})", pronoun).c_str(), "chatFont.fnt");
-
-		pronounLabel->setScale(0.3);
-		pronounLabel->setColor({ 0, 0, 0 });
-		pronounLabel->setOpacity(100);
-		pronounLabel->setPosition(this->m_backgroundLayer->getContentSize() - pronounLabel->getScaledContentSize());
-		pronounLabel->setID("pronoun-label");
-		m_mainLayer->addChild(pronounLabel);
-    }
+			Build<CCLabelBMFont>::create(pronoun.c_str(), "chatFont.fnt")
+				.scale(0.5)
+				.color(0, 0, 0)
+				.opacity(100)
+				.pos(lc->m_backgroundLayer->getContentSize() - ccp(10, 3))
+				.anchorPoint({1, 1})
+				.id("pronoun-label")
+				.parent(lc->m_mainLayer);
+		}
+	}
 };
